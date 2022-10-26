e54e54c064a99393f8539e9f3648134c152c828d
NeverEvi <thhjp.dekimasen@gmail.com>
2022-10-26 14:50:38 -0400

    add js script

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+    object_1 = {
+        name: "Yosemite Valley",
+        link: "../images/yosemite-valley.jpg"
+    },
+    object_2 = {
+        name: "Lake Louise",
+        link: "../images/lake-louise.png"
+    },
+    object_3 = {
+        name: "Bald Mountains",
+        link: "../images/bald-mountains.png"
+    },
+    object_4 = {
+        name: "Latemar",
+        link: "../images/latemar.jpg"
+    },
+    object_5 = {
+        name: "Vanoise National Park",
+        link: "../images/vanoise-national-park.png"
+    },
+    object_6 = {
+        name: "Lago di Braies",
+        link: "../images/lago-di-braies.png"
+    },
+]
